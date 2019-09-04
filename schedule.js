4ad7ff64a2d2c824b0fe8378f507f0183a1254bb
Doug McGlathery <dmcglathery@gmail.com>
2019-09-03 23:49:42 -0400

    Schedule set for second day of  school

--- schedule.js
+++ schedule.js
@@ -788,9 +788,9 @@ function getStart(block) {
 
 // next three lines for special schedules, comment out for normal days
 // issue: need to automate this
-     scheduleA = scheduleFirstDay;
-     scheduleB = scheduleFirstDay;
-     scheduleC = scheduleFirstDay;
+     scheduleA = scheduleSecondDay;
+     scheduleB = scheduleSecondDay;
+     scheduleC = scheduleSecondDay;
 var lunchMode = "A";
 if(localStorage.lunch) {
   lunchMode = localStorage.lunch;
