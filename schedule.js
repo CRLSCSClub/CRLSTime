1ff2adf97625c0d8ed4cd1c8afdee673f13ed39f
Doug McGlathery <dmcglathery@gmail.com>
2019-12-18 23:24:23 -0500

    Dec 19 X Block

--- schedule.js
+++ schedule.js
@@ -884,9 +884,9 @@ function getStart(block) {
 
 // next three lines for special schedules, comment out for normal days
 // issue: need to automate this
-//    scheduleA = scheduleHalfDay;
-//     scheduleB = scheduleHalfDay;
-//     scheduleC = scheduleHalfDay;
+    scheduleA = scheduleAX2;
+    scheduleB = scheduleBX2;
+    scheduleC = scheduleCX2;
 var lunchMode = "A";
 if(localStorage.lunch) {
   lunchMode = localStorage.lunch;
