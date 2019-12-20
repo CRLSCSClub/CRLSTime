e7bb64993ab02ab1337cf8eebb40c1ced589a04b
Doug McGlathery <dmcglathery@gmail.com>
2019-12-20 00:15:31 -0500

    Back to normal

--- schedule.js
+++ schedule.js
@@ -884,9 +884,9 @@ function getStart(block) {
 
 // next three lines for special schedules, comment out for normal days
 // issue: need to automate this
-    scheduleA = scheduleAX2;
-    scheduleB = scheduleBX2;
-    scheduleC = scheduleCX2;
+//    scheduleA = scheduleAX2;
+//    scheduleB = scheduleBX2;
+//    scheduleC = scheduleCX2;
 var lunchMode = "A";
 if(localStorage.lunch) {
   lunchMode = localStorage.lunch;
