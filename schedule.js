b6ead725d2b1e1b500ebca9520564c3154243619
Doug McGlathery <dmcglathery@gmail.com>
2020-03-09 22:04:47 -0400

    Afternoon conferences

--- schedule.js
+++ schedule.js
@@ -905,9 +905,9 @@ function getStart(block) {
 
 // next three lines for special schedules, comment out for normal days
 // issue: need to automate this
-//    scheduleA = scheduleCurrNight;
-//    scheduleB = scheduleCurrNight;
-//    scheduleC = scheduleCurrNight;
+   scheduleA = scheduleAfternoonConferences;
+   scheduleB = scheduleAfternoonConferences;
+   scheduleC = scheduleAfternoonConferences;
 var lunchMode = "A";
 if(localStorage.lunch) {
   lunchMode = localStorage.lunch;
