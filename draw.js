38671da60ac3a06f6e0f7ff0a5274edac1cca68c
Doug McGlathery <dmcglathery@gmail.com>
2024-11-01 12:33:17 -0400

    Update draw.js

--- draw.js
+++ draw.js
@@ -357,7 +357,7 @@ var dayLabel = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 var today = new Date();
 function drawDayOfWeek() {
   drawSmallCircle(lowerLeftX, lowerLeftY, smRadius, '#DDD');
-  ctx.font = "bold " + radius*0.067 + "px arial";
+  ctx.font = "bold " + radius*0.1 + "px arial";
   ctx.textBaseline="middle";
   ctx.textAlign="center";
   ctx.fillStyle = '#888';
